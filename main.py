7da9386e8e9585f2e197e592892909d8d41ea6f9
b6Ah_SHG-262-Wb <xyf.oco@gmail.com>
2024-11-24 02:01:17 +0800

    Country specific saving enable

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import pandas as pd
 import os
 from enum import Enum
 import pycountry as pc
+import numpy as np
+import logging
+import datetime
+import textwrap
+
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler = logging.FileHandler(f'logs\\{datetime.date.today()}.log', mode='w')
+handler.setFormatter(formatter)
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 
 class FileOrigin(Enum):
@@ -11,9 +23,9 @@ class FileOrigin(Enum):
 
 
 class FeaturesCompiler:
-    def __init__(self, feature_folders_path='.'):
+    def __init__(self, features_path=os.path.join(".", "features")):
         # Path to the folder containing all feature folders like 1. Mean air temperature
-        self.feature_folders_path = feature_folders_path
+        self.features_path = features_path
         # All features will be merged into this DataFrame
         self.df = pd.DataFrame(columns=["code", "year"])
         self.cannot_convert_to_alpha_3 = {}
@@ -23,10 +35,20 @@ class FeaturesCompiler:
             alpha_3 = pc.countries.lookup(country_name).alpha_3
             return alpha_3
         except LookupError:
-            self.cannot_convert_to_alpha_3[country_name] = True
-            return "ZZZZZ"
+            if country_name == "Ethiopia PDR":
+                return "ETH"
+            else:
+                self.cannot_convert_to_alpha_3[country_name] = True
+                return "ZZZZZ"
 
-    def read_feature(self, feature_name: str, file_path: str, file_origin: FileOrigin):
+    def alpha_3_to_country_name(self, alpha_3: str):
+        try:
+            country_name = pc.countries.get(alpha_3=alpha_3).name
+            return country_name
+        except Exception as e:
+            return alpha_3
+
+    def merge_feature(self, feature_name: str, file_path: str, file_origin: FileOrigin):
         try:
             df = None
             feature_name = os.path.basename(os.path.dirname(file_path))
@@ -37,8 +59,9 @@ class FeaturesCompiler:
             if file_origin == FileOrigin.DATA_WORLD_BANK:
                 df = pd.read_csv(file_path)
                 df = df.loc[:, ~df.columns.str.contains('Unnamed: 68')]
-                df.drop(columns=["Country Name", "Indicator Name",
-                        "Indicator Code"], inplace=True)
+                columns_to_drop = ["Country Name", "Indicator Name",
+                                   "Indicator Code"]
+                df.drop(columns_to_drop, inplace=True, axis=1)
                 df = pd.melt(df, id_vars=[
                              "Country Code"], value_vars=df.columns[1:], var_name="year", value_name=feature_name)
                 df.rename(columns={"Country Code": "code"}, inplace=True)
@@ -61,16 +84,20 @@ class FeaturesCompiler:
                     self.country_name_to_alpha_3)
                 df.rename(
                     columns={"Area": "code", "Year": "year", "Value": feature_name}, inplace=True)
-                df.drop(columns=["Area Code (M49)", "Domain", "Months Code", "Months",
-                                 "Domain Code", "Element Code", "Element", "Item Code", "Item", "Year Code", "Unit", "Flag", "Flag Description"], inplace=True, errors="ignore")
+                columns_to_drop = ["Area Code (M49)", "Domain", "Months Code", "Months", "Note",
+                                   "Domain Code", "Element Code", "Element", "Item Code", "Item", "Year Code", "Unit", "Flag", "Flag Description"]
+                df.drop(columns=columns_to_drop,
+                        inplace=True, errors='ignore', axis=1)
                 df["year"] = df["year"].apply(lambda x: int(x))
                 self.df = pd.merge(
                     self.df, df, on=["code", "year"], how='outer')
             else:
                 raise Exception("Unknown file origin", file_path)
-            print(df)
+            print("Merge successful:", feature_name, file_path, file_origin)
         except Exception as e:
             print("Error reading feature file", file_path, file_origin)
+            print("DataFrame shape:", self.df.shape)
+            print("Columns:", self.df.columns)
             raise e
 
     def find_origin_url(self, folder: str):
@@ -79,7 +106,6 @@ class FeaturesCompiler:
             raise Exception("URL file not found in folder", folder)
         with open(os.path.join(folder, "url.txt"), 'r') as f:
             url = f.read().strip()
-        print("URL:", url)
         if url.startswith(FileOrigin.DATA_WORLD_BANK.value):
             return FileOrigin.DATA_WORLD_BANK
         elif url.startswith(FileOrigin.CLIMATE_KNOWLEDGE_PORTAL.value):
@@ -89,29 +115,47 @@ class FeaturesCompiler:
         else:
             raise Exception("Unsupported origin URL", url)
 
+    def get_all_feature_folder_paths(self):
+        return [os.path.join(self.features_path, f) for f in os.listdir(self.features_path) if os.path.isdir(os.path.join(self.features_path, f))]
+
     def compile(self):
-        print("Starting compilation at:", f'"{self.feature_folders_path}"')
-        for feature_folder in os.listdir(self.feature_folders_path):
-            feature_folder_path = os.path.join(
-                self.feature_folders_path, feature_folder)
-            if feature_folder.startswith('.') or not os.path.isdir(feature_folder_path):
-                continue
-            print("Feature folder located:", feature_folder_path)
+        countries_to_save = ["SOM", "ETH", "KEN", "SSD", "SDN"]
+
+        # Iterate over all feature folders to merge them into a single DataFrame
+        feature_folder_paths = self.get_all_feature_folder_paths()
+        for feature_folder_path in feature_folder_paths:
             origin_url = self.find_origin_url(feature_folder_path)
             for feature in os.listdir(feature_folder_path):
                 if feature == "url.txt":
                     continue
                 feature_name = os.path.basename(feature_folder_path)
                 feature_path = os.path.join(feature_folder_path, feature)
-                self.read_feature(feature_name, feature_path, origin_url)
-        self.df.dropna(inplace=True)
-        print('Final DataFrame:', self.df)
-        print('DataFrame columns:', self.df.columns)
-        print('Countries that could not be converted to alpha-3:',
-              self.cannot_convert_to_alpha_3)
-        self.df.to_csv(os.path.join(self.feature_folders_path,
-                       "features.csv"), index=False)
+                self.merge_feature(feature_name, feature_path, origin_url)
+                self.df.drop(self.df[~self.df["code"].isin(
+                    countries_to_save)].index, inplace=True)
+
+        # Sort the columns
+        sorted_columns = sorted(self.df.columns, key=lambda x: (0, x) if x in [
+            "code", "year"] else (1, int(x.split(".")[0])))
+        self.df = self.df[sorted_columns]
+
+        # Save the individual country features
+        features_csv_folder = os.path.join(".", "features_csv")
+        for country in countries_to_save:
+            df = self.df.drop(self.df[self.df["code"] != country].index)
+            df.to_csv(os.path.join(features_csv_folder,
+                      f"{country}.csv"), index=False)
+
+        # Save the aggregated features
+        print('Aggregrated DataFrame shape:', self.df.shape)
+        print('Features:', self.df.columns)
+        wrapped_message = textwrap.fill(f"Cannot convert to alpha 3: {
+                                        list(self.cannot_convert_to_alpha_3.keys())}", width=50)
+        logger.debug(wrapped_message)
+
+        self.df.to_csv(os.path.join(features_csv_folder,
+                       "aggregrated.csv"), index=False)
 
 
-features_compiler = FeaturesCompiler(os.path.join(".", "features"))
+features_compiler = FeaturesCompiler()
 features_compiler.compile()
